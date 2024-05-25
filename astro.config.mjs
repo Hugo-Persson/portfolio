7f44160ebf54d74c78779eea7f26b59cc55c0453
Hugo Persson <hugo.e.persson@gmail.com>
2024-05-25 23:28:05 +0200

    Plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+import expressiveCode from "astro-expressive-code";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://hugopersson.com",
+  integrations: [tailwind(), expressiveCode(), sitemap()],
   output: "server",
   adapter: cloudflare({}),
 });
+
